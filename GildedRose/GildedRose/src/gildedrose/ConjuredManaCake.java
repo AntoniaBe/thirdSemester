b3b9c8c46e8d42b4e4e4b62cc551dfb85560140b
Antonia Berger <AntoniaBerger@gmx.net>
2016-01-18 21:22:09 +0100

    Initial Upload

---
+++ GildedRose/GildedRose/src/gildedrose/ConjuredManaCake.java
@@ -0,0 +1,10 @@
+package gildedrose;
+
+public class ConjuredManaCake extends UQS{
+	@ Override
+	public Item updateQualitiy(Item item) {
+			item.setQuality(checkQuality(item.getQuality() - 2));
+			return item;
+	}
+
+}
---
+++ GildedRose/GildedRose/src/gildedrose/Sulfuras.java
@@ -0,0 +1,13 @@
+package gildedrose;
+
+public class Sulfuras extends UQS {
+	@Override
+	public Item updateQualitiy(Item item) {
+		return item;
+	}
+
+	@Override
+	public Item updateSellIn(Item item) {
+		return item;
+	}
+}
